6e119f8c5fcde40f3c83d1a1cdef590c2e0b0f23
Tim <tim@webkeller.ch>
2019-06-28 10:25:40 +0200

    update services page for better responsiveness

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="col-md-12 mb-5">
-        <div class="d-flex justify-content-center">
-            <h1>Our websites and applications</h1>
-        </div>
-        <hr>
+<div class="col-md-12 mb-5">
+<div class="d-flex justify-content-center">
+    <h1>Our websites and applications</h1>
+</div>
+<hr>
+<div class="row">
+    <div class="col-lg-8 col-md-12">
         <div class="row">
-            <div class="col-md-4 col-12 mb-4">
+            <div class="col-md-4 col-6 mb-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">MovieScore <i class="fas fa-video"></i></h5>
@@ -14,33 +16,31 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 col-12 mb-4">
+            <div class="col-md-4 col-6 mb-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Minecraft Server <i class="fas fa-gamepad"></i></h5>
                         <p class="card-text">We have a vanilla Minecraft server running on Version 1.14.3. Hit me up if you want to join</p>
-                        <a href="#">IP: scorewinner.ch:25565</a>
+                        <a href="#">IP address: scorewinner.ch:25565</a>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 col-12 mb-4">
+            <div class="col-md-4 col-6 mb-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Unturned Server <i class="fas fa-gamepad"></i></h5>
                         <p class="card-text">We have a Unturned server running on the newest Version on the map "Russia". Hit me up if you want to join.</p>
-                        <a href="#">IP: scorewinner.ch:27015</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-12 mb-4 discord">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Discord Server <i class="fab fa-discord"></i></h5>
-                        <p class="card-text">We have also a Discord server. Hit me up if you want to join.</p>
-                        <iframe src="https://discordapp.com/widget?id=355686076003778561&amp;theme=dark" class="discord" allowtransparency="true" frameborder="0"></iframe>
+                        <a href="#">IP address: scorewinner.ch:27015</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="col-lg-4 col-md-12">
+        <h5 class="card-title">Discord Server <i class="fab fa-discord"></i></h5>
+        <p class="card-text">We have also a Discord server. Hit me up if you want to join.</p>
+        <iframe src="https://discordapp.com/widget?id=355686076003778561&amp;theme=dark" class="discord" allowtransparency="true" frameborder="0"></iframe>
+    </div>
+</div>
+</div>
 </template>
